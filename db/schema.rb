cfc4b39b4677d859ec2e35f2c2dc8eb390a28e5f
laowang <904885508@qq.com>
2022-11-02 09:56:42 +0800

    创建validationController

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_080725) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_013525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "validation_codes", force: :cascade do |t|
+    t.string "email"
+    t.string "kind"
+    t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
