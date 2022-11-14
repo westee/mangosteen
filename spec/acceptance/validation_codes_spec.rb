e7a778a8164952646ae94f8cb89121d05e9b5d61
laowang <904885508@qq.com>
2022-11-14 16:10:10 +0800

    解决文档json显示问题

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "验证码" do
+
   post "/api/v1/validation_codes" do
+    # header "Content-Type", "application/json"
+    parameter :email, "邮件接收方email", type: :string, with_example: true
+    let(:email) { "email@example.com" }
     example "发送验证码" do
       do_request
-
       expect(status).to eq 200
+      res = JSON.parse(response_body)
+      expect(res[:code]).to eq nil
+
     end
   end
 end
