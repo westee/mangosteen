6deb6ef783809511307b82ef126d4a0e48d81bc1
laowang <904885508@qq.com>
2022-11-15 23:26:49 +0800

    完成 me#show 接口

---
+++ spec/requests/api/v1/mes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "me", type: :request do
+  describe "获取当前用户" do
+    it "登录后成功获取" do
+      user = User.create email: 'xxx@qq.com'
+      post '/api/v1/session', params: {email: 'xxx@qq.com', code: '123456'}
+      json = JSON.parse response.body
+      jwt = json['jwt']
+    
+      get '/api/v1/me', headers: {'Authorization': "Bearer #{jwt}"}
+      expect(response).to have_http_status(200)
+      json = JSON.parse response.body
+      
+      # expect(json['jwt']).to be_a(String)
+    end
+  end
+end
