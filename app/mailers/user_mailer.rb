1889e0f79ec8d810cfae0a44b9f2e6ac1ae7cab4
laowang <904885508@qq.com>
2022-11-15 11:05:41 +0800

    使用最后一次发送的email-code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   # default from: 'notifications@example.com'
 
   def welcome_email(email)
-    validationCode = ValidationCode.find_by_email(email)
+    validationCode = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validationCode.code
     mail(to: email, subject: "山竹记账验证码")
   end
--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,6 +1,9 @@
 class ValidationCode < ApplicationRecord
   validates :email, presence: true
 
+	before_create :generate_code
+	after_create :send_email
+
 	def generate_code
     self.code = SecureRandom.random_number.to_s[2..7]
   end
