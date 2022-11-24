14c5d07cf0765d9a76f0e545254bb299cc424fb2
laowang <904885508@qq.com>
2022-11-24 15:37:22 +0800

    增加balance和summary接口

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -27,4 +27,55 @@ class Api::V1::ItemsController < ApplicationController
       render json: { errors: item.errors, uid: request.env['current_user_id'] }, status: 422
     end
   end
+
+  def balance
+    current_user_id = request.env["current_user_id"]
+    return head :unauthorized if current_user_id.nil?
+    items = Item.where({ user_id: current_user_id })
+      .where({ happen_at: params[:happen_after]..params[:happen_before] })
+    income_items = []
+    expenses_items = []
+    items.each {|item|
+      if item.kind === 'income'
+        income_items << item
+      else
+        expenses_items << item
+      end
+    }
+    income = income_items.sum(&:amount)
+    expenses = expenses_items.sum(&:amount)
+    render json: { income: income, expenses: expenses, balance: income - expenses }
+  end
+
+  def summary
+    hash = Hash.new
+    items = Item
+      .where(user_id: request.env["current_user_id"])
+      .where(kind: params[:kind])
+      .where(happen_at: params[:happened_after]..params[:happened_before])
+    items.each do |item|
+      if params[:group_by] == "happen_at"
+        key = item.happen_at.in_time_zone("Beijing").strftime("%F")
+        hash[key] ||= 0
+        hash[key] += item.amount
+      else
+        item.tag_ids.each do |tag_id|
+          key = tag_id
+          hash[key] ||= 0
+          hash[key] += item.amount
+        end
+      end
+    end
+    groups = hash
+      .map { |key, value| { "#{params[:group_by]}": key, amount: value } }
+    if params[:group_by] == "happen_at"
+      groups.sort! { |a, b| a[:happen_at] <=> b[:happen_at] }
+    elsif params[:group_by] == "tag_id"
+      groups.sort! { |a, b| b[:amount] <=> a[:amount] }
+    end
+    render json: {
+      groups: groups,
+      total: items.sum(:amount),
+    }
+  end
 end
