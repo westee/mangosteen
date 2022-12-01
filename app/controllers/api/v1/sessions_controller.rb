cea035ed3f05325d632e47d20bc0d2c082fc5490
laowang <904885508@qq.com>
2022-12-01 21:31:21 +0800

    修改登录bug

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,9 @@
 require 'jwt'
 class Api::V1::SessionsController < ApplicationController
   def create 
-    Session.new params.permit(:email, :code)
+    session = Session.new params.permit(:email, :code)
     if session.valid?
-      User.find_or_create_by email: session.email
+      user = User.find_or_create_by email: session.email
       render json: { jwt: user.generate_token }
     else
       render json: { errors: session.errors},status: :unprocessable_entity
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   # default from: 'notifications@example.com'
 
   def welcome_email(email)
+    p '------------------邮件--------------'
+    p email
     validationCode = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validationCode.code
     mail(to: email, subject: "山竹记账验证码")
